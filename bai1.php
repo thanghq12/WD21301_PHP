13bb222731d4a2b8dc77bcce264c76e1c4b91508
thang hoang <thanghq12@fpt.edu.vn>
2026-01-07 13:10:11 +0700

    update

---
+++ bai1.php
@@ -0,0 +1,142 @@
+<?php
+echo "Hello world <br>";
+echo "Thanghq12 4444 <br>";
+// Khai báo biến trong php phải có $ ở đầu biến 
+
+$a = 10;
+$b = 20;
+$c = $a + $b;
+echo "tong la ".$c;
+// khai báo biến chieudai = 50 
+// chieurong = 70 
+// hien thi ra chu vi va dien tich HCN  
+$chieudai = 50;
+$chieurong = 70;
+$chuvi = ($chieudai + $chieurong) * 2;
+$dientich = $chieudai * $chieurong;
+echo "Chu vi là".$chuvi."<br>";
+echo "Diện tích là".$dientich."<br>";
+// Bài 1. Xuất thông tin cá nhân
+
+// Tạo file bai1.php
+
+// Dùng echo để hiển thị:
+
+// Họ tên
+
+// Lớp
+
+// Ngành học
+// 👉 Mỗi thông tin hiển thị trên 1 dòng
+
+// Bài 2. Tính tổng – hiệu – tích – thương
+
+// Khai báo 2 biến $a, $b
+
+// Thực hiện:
+
+// Tổng
+
+// Hiệu
+
+// Tích
+
+// Thương
+
+// Hiển thị kết quả ra màn hình
+
+// Bài 3. Tính chu vi & diện tích hình vuông
+
+// Khai báo:
+
+// $canh = 25
+
+// Tính:
+
+// Chu vi
+
+// Diện tích
+
+// In kết quả ra màn hình
+
+// Bài 4. Tính chu vi & diện tích hình tròn
+
+// Khai báo:
+
+// $r = 10
+
+// $pi = 3.14
+
+// Tính:
+
+// Chu vi
+
+// Diện tích hình tròn
+
+// Hiển thị kết quả
+
+// Bài 5. Tính tổng 3 số
+
+// Khai báo 3 biến $x, $y, $z
+
+// Tính tổng 3 số
+
+// In kết quả ra màn hình
+
+// Bài 6. Tính điểm trung bình
+
+// Khai báo:
+
+// $toan
+
+// $ly
+
+// $hoa
+
+// Tính điểm trung bình
+
+// Hiển thị kết quả
+
+// Bài 7. Tính lương nhân viên
+
+// Khai báo:
+
+// $luongNgay = 300000
+
+// $soNgayLam = 26
+
+// Tính tổng lương
+
+// Hiển thị:
+// 👉 “Tổng lương của nhân viên là: … VNĐ”
+
+// Bài 8. Tính tiền mua hàng
+
+// Khai báo:
+
+// $tenSanPham
+
+// $donGia
+
+// $soLuong
+
+// Tính thành tiền
+
+// Hiển thị:
+
+// Tên sản phẩm
+
+// Thành tiền
+
+// Bài 9. Tính tuổi
+
+// Khai báo:
+
+// $namSinh
+
+// $namHienTai
+
+// Tính tuổi
+
+// In ra màn hình
+?>
