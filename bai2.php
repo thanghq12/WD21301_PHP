44b515f58061b1375c05293668b15781336a2428
thang hoang <thanghq12@fpt.edu.vn>
2026-01-12 12:59:14 +0700

    update

--- bai2.php
+++ bai2.php
@@ -5,13 +5,36 @@ $a = 10;
 // } else {
 //     echo "Đây là số lẻ";
 // }
-if($a > 20) {
-    echo "hihi";
-} elseif ($a >15) {
-    echo "haha";
-} elseif ($a > 9) {
-    echo "hoho";
-} else {
-    echo "lêu lêu";
+// if($a > 20) {
+//     echo "hihi";
+// } elseif ($a >15) {
+//     echo "haha";
+// } elseif ($a > 9) {
+//     echo "hoho";
+// } else {
+//     echo "lêu lêu";
+// }
+// $luaChon = 5;
+// switch($luaChon) {
+//     case 1:
+//         echo "aaa";
+//     break;
+//     case 2:
+//         echo "bbb";
+//     break;
+//     case 5: 
+//         echo "ccc";
+//     break;
+//     default:
+//         echo "ddd";
+//     break;
+// }
+// lặp từ hello world 10 lần bằng php 
+for($i = 0;$i < 10; $i ++) {
+    // echo "hello world <br>";
+    if($i % 2 == 0 ) {
+        echo $i."<br>";
+    }
 }
+// hiển thị các số chẵn từ 0 -> 10 
 ?>
