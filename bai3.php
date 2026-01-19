21f044e3d9451fed3a0730c4f2f46c4e144f0309
thang hoang <thanghq12@fpt.edu.vn>
2026-01-19 13:12:13 +0700

    update

--- bai3.php
+++ bai3.php
@@ -6,18 +6,30 @@
     <title>Document</title>
 </head>
 <body>
-    <form method="GET">
+    <form method="POST">
         chiều dài <input type="text" name="chieudai" /></br>
         chiều rộng  <input type="text" name="chieurong" /></br>
+        lựa chọn 
+        <select name="luachon">
+            <option value="1">Tính Chu Vi</option>
+            <option value="2">Tính Diện Tích</option>
+        </select>
         <input type="submit" name="tinh" value="tinh" />
     </form>
     <?php
     // kiểm tra xem có tồn tại nut hay không 
-    if(isset($_GET['tinh'])) {// người dùng thực hiện ấn nút 
-        $chieudai = $_GET['chieudai']; // lấy dữ liệu từ input có name là soa
-        $chieurong = $_GET['chieurong']; // lấy dữ liệu từ input có name là sob
-        $dientich = $chieudai * $chieurong;
-        echo $dientich; 
+    if(isset($_POST['tinh'])) {// người dùng thực hiện ấn nút 
+        $chieudai = $_POST['chieudai']; // lấy dữ liệu từ input có name là soa
+        $chieurong = $_POST['chieurong']; // lấy dữ liệu từ input có name là sob
+        $luachon = $_POST['luachon'];
+        if($luachon == 1) {
+            $chuvi = ($chieudai + $chieurong)*2;
+            echo $chuvi;
+        } else if ($luachon == 2) {
+            $dientich = $chieudai * $chieurong;
+            echo $dientich;
+        }
+        // echo $dientich; 
     }
     ?>
 </body>
