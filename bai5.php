057d9bc8c2ca152d77d4db17772d8d959deea9d1
thang hoang <thanghq12@fpt.edu.vn>
2026-01-21 12:51:12 +0700

    update

---
+++ bai5.php
@@ -0,0 +1,16 @@
+<?php
+// mảng là nơi lưu trữ nhiều phần tử/giá trị có cùng kiểu dữ liệu 
+// mảng gồm 2 phần (vị trí,giá trị)
+// khai báo mảng 
+$mang = [2,3,4,5,6];
+// lấy ra phần tử 4 
+for($i = 0;$i < count($mang);$i++) {
+    echo $mang[$i]."<br>";
+}
+
+// echo $mang[1]."<br>";
+// echo $mang[2]."<br>";
+// echo $mang[3]."<br>";
+// echo $mang[4]."<br>";
+// lấy ra tất các phần tử trong mảng 
+?>
