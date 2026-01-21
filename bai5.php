4b2fdcbe03fa6f5f2511b5da93b5a4d66c15d2ea
thang hoang <thanghq12@fpt.edu.vn>
2026-01-21 12:53:24 +0700

    update

--- bai5.php
+++ bai5.php
@@ -4,13 +4,20 @@
 // khai báo mảng 
 $mang = [2,3,4,5,6];
 // lấy ra phần tử 4 
+// duyệt mảng // hiển thị ra các phần tử trong mảng 
 for($i = 0;$i < count($mang);$i++) {
     echo $mang[$i]."<br>";
 }
-
+//hiển thị ra các phần tử là số chẵn trong mảng 
+//tính tổng các phần tử trong mảng 
+// tìm giá trị lớn nhất trong mảng 
+//tìm giá trị nhỏ nhất trong mảng 
+// hiển thị những phần tử trong mảng là ước của 18. 
 // echo $mang[1]."<br>";
 // echo $mang[2]."<br>";
 // echo $mang[3]."<br>";
 // echo $mang[4]."<br>";
-// lấy ra tất các phần tử trong mảng 
+// lấy ra tất các phần tử trong mảng
+///////ddddddddddd 
+$aaaaa;
 ?>
