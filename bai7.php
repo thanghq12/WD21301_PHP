1cf1b8db8e22cf334f5f3fa94f1dd699ff08bd28
thang hoang <thanghq12@fpt.edu.vn>
2026-01-30 13:22:48 +0700

    update

---
+++ bai7.php
@@ -0,0 +1,33 @@
+<?php 
+//mảng liên hợp 2 chiều 
+//được sử dụng để lưu và chung truyển dữ liệu quan trọng nhất trong PHP 
+//ví dụ 
+$mangSV = [
+    ["ten_sv"=>"Thang","nam_sinh"=>1996], // mỗi 1 mảng liên hợp tương ứng với 1 thông tin của sv 
+    ["ten_sv"=>"Đại","nam_sinh"=>2005],
+    ["ten_sv"=>"Minh","nam_sinh"=>2003],
+    ["ten_sv"=>"Ngân","nam_sinh"=>2005]
+];
+// cách 1 for thường 
+for($i = 0; $i < count($mangSV); $i ++) { // count($mangSV) -> trả về tổng số phần tử của mảng 
+    echo $mangSV[$i]["ten_sv"]."-".$mangSV[$i]["nam_sinh"]."<br>";
+}
+//các 2 dùng foreach 
+foreach($mangSV as $key => $value)
+{
+    echo $value["ten_sv"]."-".$value["nam_sinh"]."<br>";
+} 
+
+?>
+<table border="1"> 
+    <tr>
+        <td>Ten Sinh Vien</td>
+        <td>Nam sinh</td>
+    </tr>
+    <?php foreach($mangSV as $key=>$value) {?>
+    <tr>
+        <td><?php echo $value["ten_sv"]; ?></td>
+        <td><?php echo $value["nam_sinh"]; ?></td>
+    </tr>
+    <?php } ?>
+</table>
